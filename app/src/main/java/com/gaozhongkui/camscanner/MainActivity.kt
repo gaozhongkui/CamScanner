d316b60dd8b7fd855e10a455dd348e7c40adac97
gaozhongkui <gaozhongkui@meetstarinc.com>
2024-04-25 16:12:50 +0800

    修改：临时提交

--- app/src/main/java/com/gaozhongkui/camscanner/MainActivity.kt
+++ app/src/main/java/com/gaozhongkui/camscanner/MainActivity.kt
@@ -1,55 +1,50 @@
 package com.gaozhongkui.camscanner
 
+import android.Manifest
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.gaozhongkui.camscanner.ui.theme.CamScannerTheme
+import androidx.core.app.ActivityCompat
+import org.opencv.android.CameraBridgeViewBase
 import org.opencv.android.OpenCVLoader
+import org.opencv.core.Mat
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), CameraBridgeViewBase.CvCameraViewListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CamScannerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+
+        setContentView(R.layout.activity_main)
+
+
         if (OpenCVLoader.initLocal()) {
             Log.d(TAG, "onCreate() 加载成功")
         } else {
             Log.d(TAG, "onCreate() 加载失败")
         }
 
+        val mOpenCvCameraView = findViewById(R.id.tutorial1_activity_java_surface_view) as? CameraBridgeViewBase
+        mOpenCvCameraView?.setCvCameraViewListener(this@MainActivity)
+        mOpenCvCameraView?.enableView()
+
+        Log.d(TAG, "onCreate() called with: $mOpenCvCameraView")
+
+        ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.CAMERA),100)
+    }
+
+    override fun onCameraViewStarted(width: Int, height: Int) {
+        Log.d(TAG, "onCameraViewStarted() called with: width = $width, height = $height")
+    }
+
+    override fun onCameraViewStopped() {
+        Log.d(TAG, "onCameraViewStopped() called")
+    }
+
+    override fun onCameraFrame(inputFrame: Mat): Mat {
+        return inputFrame.diag()
     }
 
     companion object {
         private const val TAG = "MainActivity"
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CamScannerTheme {
-        Greeting("Android")
-    }
 }
